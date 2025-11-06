ec5c4ee999d474005ae215b19fdcc12fc2235fa3
ArmanAshutosh <armanashutosh9@gmail.com>
2025-11-07 00:41:42 +0530

    q86.c

---
+++ q86.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, len = 0, flag = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while (str[len] != '\0')
+        len++;
+
+    for (i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - i - 1]) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("The string is a palindrome.");
+    else
+        printf("The string is not a palindrome.");
+
+    return 0;
+}
