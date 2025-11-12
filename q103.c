1634227e7ec2d50eb36d2b8fd597746cfeb81283
ArmanAshutosh <armanashutosh9@gmail.com>
2025-11-13 00:12:18 +0530

    q103.c

---
+++ q103.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, leftSum, rightSum, pivot = -1;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < n; i++) {
+        leftSum = 0;
+        rightSum = 0;
+        for (j = 0; j < i; j++)
+            leftSum += arr[j];
+        for (j = i + 1; j < n; j++)
+            rightSum += arr[j];
+        if (leftSum == rightSum) {
+            pivot = i;
+            break;
+        }
+    }
+    printf("%d\n", pivot);
+    return 0;
+}
