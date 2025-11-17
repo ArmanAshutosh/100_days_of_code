7116cce1a23593e94fa45250ce5465b335df4d5a
ArmanAshutosh <armanashutosh9@gmail.com>
2025-11-17 23:54:41 +0530

    q111.c

---
+++ q111.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+    for (int i = 0; i <= n - k; i++) {
+        int firstNeg = 0;
+
+        for (int j = i; j < i + k; j++) {
+            if (arr[j] < 0) {
+                firstNeg = arr[j];
+                break;
+            }
+        }
+
+        printf("%d ", firstNeg);
+    }
+
+    return 0;
+}
