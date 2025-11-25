454deb98aeeefd3f8a8b00db523c971fc8cc9a0c
ArmanAshutosh <armanashutosh9@gmail.com>
2025-11-26 00:59:19 +0530

    q139.c

---
+++ q139.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+enum Colors {
+    RED,
+    GREEN,
+    BLUE
+};
+
+int main() {
+    printf("RED = %d\n", RED);
+    printf("GREEN = %d\n", GREEN);
+    printf("BLUE = %d\n", BLUE);
+    return 0;
+}
