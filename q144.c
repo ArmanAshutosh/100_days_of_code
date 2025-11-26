60bc62c71dde6664cf9e9f52143bfa631f8ad571
ArmanAshutosh <armanashutosh9@gmail.com>
2025-11-26 22:54:58 +0530

    q144.c

---
+++ q144.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+void printStudent(struct Student s) {
+    printf("Name: %s\n", s.name);
+    printf("Roll: %d\n", s.roll);
+    printf("Marks: %.2f\n", s.marks);
+}
+
+int main() {
+    struct Student s1;
+
+    printf("Enter name, roll, marks: ");
+    scanf("%s %d %f", s1.name, &s1.roll, &s1.marks);
+
+    printStudent(s1);
+
+    return 0;
+}
